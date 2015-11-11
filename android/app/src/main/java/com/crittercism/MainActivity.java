59aa7702ac5d54cfc023ce65813e7f04b833fa4a
David Shirley <dshirley@crittercism.com>
2015-11-11 10:28:41 -0800

    Add Apache 2.0 license to beginning of all .java files

--- android/app/src/main/java/com/crittercism/MainActivity.java
+++ android/app/src/main/java/com/crittercism/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Crittercism
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.crittercism;
 
 import android.os.Bundle;
@@ -28,7 +44,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Crittercism.initialize(getApplicationContext(), "YOUR APP ID GOES HERE");
+        Crittercism.initialize(getApplicationContext(), "558ebe087bdd4942113b413f");
         setContentView(R.layout.activity_work);
 
         this.viewPager = (ViewPager)findViewById(R.id.pager);
